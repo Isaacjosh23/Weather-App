8d272b2ecf0acb0b4fd78dc151206855e1c9bd7d
Ebhamen Joshua <ebhamenjoshua@gmail.com>
2025-06-05 14:49:26 +0100

    final weather codes

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState, useMemo } from "react";
 import "./Weather.css";
 import search_icon from "../Assets/img/search.png";
 import clear_icon from "../Assets/img/clear.png";
@@ -21,72 +21,65 @@ export default function Weather() {
 
   const [searchCity, setSearchCity] = useState("");
 
-  const allIcons = {
-    "01d": clear_icon,
-    "01n": clear_night,
-    "02d": cloud_icon,
-    "02n": cloud_night,
-    "03d": cloud_icon,
-    "03n": cloud_icon,
-    "04d": drizzle_icon,
-    "04n": drizzle_night,
-    "09d": rain_icon,
-    "09n": rain_night,
-    "10d": rain_icon,
-    "10n": rain_night,
-    "13d": snow_icon,
-    "13n": snow_night,
-  };
+  const allIcons = useMemo(
+    () => ({
+      "01d": clear_icon,
+      "01n": clear_night,
+      "02d": cloud_icon,
+      "02n": cloud_night,
+      "03d": cloud_icon,
+      "03n": cloud_icon,
+      "04d": drizzle_icon,
+      "04n": drizzle_night,
+      "09d": rain_icon,
+      "09n": rain_night,
+      "10d": rain_icon,
+      "10n": rain_night,
+      "13d": snow_icon,
+      "13n": snow_night,
+    }),
+    []
+  );
 
-  const search = async (city) => {
-    if (!city) {
-      alert("Please enter a city name");
-      return;
-    }
-
-    try {
-      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_ID}`;
-
-      const response = await fetch(url);
-
-      const data = await response.json();
-
-      if (!response.ok) {
-        alert(data.message);
+  const search = useCallback(
+    async (city) => {
+      if (!city) {
+        alert("Please enter a city name");
         return;
       }
-
-      console.log(data);
-
-      const icon = allIcons[data.weather[0].icon] || clear_icon;
-
-      setWeatherData({
-        humidity: data.main.humidity,
-        windSpeed: data.wind.speed,
-        temperature: Math.floor(data.main.temp),
-        location: data.name,
-        icon: icon,
-      });
-    } catch (error) {
-      setWeatherData(false);
-      console.error("Error fetching weather data");
-    }
-  };
+      try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${process.env.REACT_APP_ID}`;
+        const response = await fetch(url);
+        const data = await response.json();
+        if (!response.ok) {
+          alert(data.message);
+          return;
+        }
+        const icon = allIcons[data.weather[0].icon] || clear_icon;
+        setWeatherData({
+          humidity: data.main.humidity,
+          windSpeed: data.wind.speed,
+          temperature: Math.floor(data.main.temp),
+          location: data.name,
+          icon: icon,
+        });
+      } catch (error) {
+        setWeatherData(false);
+        console.error("Error fetching weather data");
+      }
+    },
+    [allIcons]
+  );
 
   function handleSearch(e) {
     e.preventDefault();
-
     search(inputRef.current.value);
-
-    // inputRef.current.value = "";
-
     setSearchCity("");
   }
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
     search("London");
-  }, []);
+  }, [search]);
 
   return (
     <div className="weather">
